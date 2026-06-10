3ab9c8b04d494310827bfe66ef15f4c71f37a027
SandandGlass <sandandglass0419@gmail.com>
2026-06-10 20:26:25 +0900

    hide

--- JengaSim/Assets/Experiment/Utility.cs
+++ JengaSim/Assets/Experiment/Utility.cs
@@ -64,7 +64,7 @@ namespace Experiment
         public Dictionary<string, string[]> FallenBuffer = new();
         public Dictionary<string, string[]> SurvivedBuffer = new();
     
-        public const string SearchDir = "/home/cinnamon/Projects/Zenga/DepthSearch/";
+        public const string SearchDir = "/";
         public const string Indexes = "board,cog_axis,cog_value,unity_time,unity_state,unity_axis,unity_mav,unity_mlv\n";
 
         public static string GetMeasurementFilePath(int depth, int sector, bool fallen) => fallen
